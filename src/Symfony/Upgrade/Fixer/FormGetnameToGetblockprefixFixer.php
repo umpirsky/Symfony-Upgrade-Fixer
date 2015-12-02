edc722a389a5facacdc6d11c28ae1d105497710e
Kristijan Husak <husakkristijan@gmail.com>
2015-12-02 09:36:58 +0100

    Remove unneeded condition, update phpdoc, change description.

--- src/Symfony/Upgrade/Fixer/FormGetnameToGetblockprefixFixer.php
+++ src/Symfony/Upgrade/Fixer/FormGetnameToGetblockprefixFixer.php
@@ -7,14 +7,8 @@ use Symfony\CS\Tokenizer\Tokens;
 
 class FormGetnameToGetblockprefixFixer extends FormTypeFixer
 {
-
     /**
-     * Fixes a file.
-     *
-     * @param \SplFileInfo $file A \SplFileInfo instance
-     * @param string       $content The file content
-     *
-     * @return string The fixed file content
+     * @inheritdoc
      */
     public function fix(\SplFileInfo $file, $content)
     {
@@ -41,24 +35,16 @@ class FormGetnameToGetblockprefixFixer extends FormTypeFixer
     private function fixGetNameMethod(Tokens $tokens)
     {
         $matchedTokens = $this->matchGetNameMethod($tokens);
-        if (null === $matchedTokens) {
-            return;
-        }
-
         $matchedIndexes = array_keys($matchedTokens);
         $matchedIndex = $matchedIndexes[count($matchedIndexes) - 3];
         $matchedTokens[$matchedIndex]->setContent('getBlockPrefix');
     }
 
     /**
-     * Returns the description of the fixer.
-     *
-     * A short one-line description of what the fixer does.
-     *
-     * @return string The description of the fixer
+     * @inheritdoc
      */
     public function getDescription()
     {
-        return 'The method FormTypeInterface::getName() was deprecated and will be removed in Symfony 3.0. You should now implement FormTypeInterface::getBlockPrefix() instead.';
+        return 'The method FormTypeInterface::getName() was deprecated, you should now implement FormTypeInterface::getBlockPrefix() instead.';
     }
 }
